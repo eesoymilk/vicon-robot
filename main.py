8778e999311e36191192a082bb9c5a127d604feb
Xiaoying Yang <109782231+xyyhci@users.noreply.github.com>
2025-02-25 14:13:04 -0800

    feat: hardcoded pos

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from robot_controller.robot_controller import RobotController
 
-
 def main() -> None:
     controller = RobotController()
     controller.start()
+    # controller.hard_coded_grasp()
 
 
 if __name__ == "__main__":
